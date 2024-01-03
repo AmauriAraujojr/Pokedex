465fd200a5c87c69a2d66b905f1594f068dafc76
Amauri Araujo <amauriaraujojr@yahoo.com.br>
2024-01-03 06:34:28 -0300

    feat: darkMode Butoon

--- assets/js/render.js
+++ assets/js/render.js
@@ -7,8 +7,8 @@ const createPokemon = (pokemon) => {
   const pokemonContainer = document.createElement("div");
   const pokemonTypes = document.createElement("ol");
   const pokemonImg = document.createElement("img");
-  const pokemonTitleContainer=document.createElement("div")
-  const pokeballImg=document.createElement("img")
+  const pokemonTitleContainer = document.createElement("div");
+  const pokeballImg = document.createElement("img");
 
   pokemon.types.map((type) => {
     const pokemonTypeOne = document.createElement("li");
@@ -20,7 +20,8 @@ const createPokemon = (pokemon) => {
   pokemonName.innerText = `${pokemon.name}`;
   pokemonNumber.innerText = `#${pokemon.number}`;
   pokemonImg.src = pokemon.photo;
-  pokeballImg.src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/57541cc3-da0f-40dc-94ec-49b954d5805a/d93bs8z-cc196bbd-da41-4167-9fec-798ae39fa542.png/v1/fill/w_894,h_894/pokeball_logo_by_pedronex_d93bs8z-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwMCIsInBhdGgiOiJcL2ZcLzU3NTQxY2MzLWRhMGYtNDBkYy05NGVjLTQ5Yjk1NGQ1ODA1YVwvZDkzYnM4ei1jYzE5NmJiZC1kYTQxLTQxNjctOWZlYy03OThhZTM5ZmE1NDIucG5nIiwid2lkdGgiOiI8PTEwMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.X38yrINPG4Dj5tIRHsUm5tGhxdvm77QMyBeOREfr6CY"
+  pokeballImg.src =
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/57541cc3-da0f-40dc-94ec-49b954d5805a/d93bs8z-cc196bbd-da41-4167-9fec-798ae39fa542.png/v1/fill/w_894,h_894/pokeball_logo_by_pedronex_d93bs8z-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwMCIsInBhdGgiOiJcL2ZcLzU3NTQxY2MzLWRhMGYtNDBkYy05NGVjLTQ5Yjk1NGQ1ODA1YVwvZDkzYnM4ei1jYzE5NmJiZC1kYTQxLTQxNjctOWZlYy03OThhZTM5ZmE1NDIucG5nIiwid2lkdGgiOiI8PTEwMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.X38yrINPG4Dj5tIRHsUm5tGhxdvm77QMyBeOREfr6CY";
 
   pokemonCard.className = `pokemon ${pokemon.type}`;
 
@@ -29,22 +30,21 @@ const createPokemon = (pokemon) => {
   pokemonContainer.className = "pokemonDetail";
   pokemonTypes.className = "pokemonTypes";
   pokemonImg.className = "pokemonImg";
-  pokeballImg.className="pokeballImg";
-  pokemonTitleContainer.className="pokemonTitleContainer"
+  pokeballImg.className = "pokeballImg";
+  pokemonTitleContainer.className = "pokemonTitleContainer";
 
-  pokemonTitleContainer.append(pokemonName,pokemonNumber,pokeballImg)
+  pokemonTitleContainer.append(pokemonName, pokemonNumber, pokeballImg);
   pokemonContainer.append(pokemonTypes, pokemonImg);
-  pokemonCard.append( pokemonTitleContainer, pokemonContainer);
+  pokemonCard.append(pokemonTitleContainer, pokemonContainer);
 
   return pokemonCard;
 };
 
-export const renderPokemon = async (limit,offset) => {
+export const renderPokemon = async (limit, offset) => {
   const ol = document.querySelector("#pokemonList");
   ol.innerHTML = "";
-  const response = await getPokemons(limit,offset);
+  const response = await getPokemons(limit, offset);
 
-  
   const listPokemon = response;
 
   listPokemon.map((pokemon) => {
@@ -57,25 +57,30 @@ const loadMorePokemon = () => {
   const loadButton = document.querySelector("#loadMoreButton");
 
   let limit = 3;
-  let offset= 0;
+  let offset = 0;
   loadButton.addEventListener("click", () => {
-
-limit+=3
-    renderPokemon(limit,offset);
+    limit += 3;
+    renderPokemon(limit, offset);
   });
 };
 
 loadMorePokemon();
 
-const darkMode=()=>{
-  const buttonDark=document.querySelector(".darkMode")
-  const backGround=document.querySelector(".content")
+const darkMode = () => {
+  const buttonDark = document.querySelector(".darkMode");
+  const backGround = document.querySelector(".content");
+  const icon = document.querySelector(".icon");
 
-  buttonDark.addEventListener("click",()=>{
-  backGround.classList.toggle("contentDark")
+  buttonDark.addEventListener("click", () => {
+    backGround.classList.toggle("contentDark");
+    buttonDark.classList.toggle("ligthMode");
+    if (buttonDark.className != "darkMode") {
+      icon.src =
+        "https://static-00.iconduck.com/assets.00/mode-light-icon-2048x2048-no286vfd.png";
+    } else {
+      icon.src = "https://static.thenounproject.com/png/4498020-200.png";
+    }
+  });
+};
 
-  })
-}
-
-darkMode()
-
+darkMode();
